4371ea90ef6c085c4ade8f8876ae94cb2f2850d6
CaoHuuKhuongDuy <khuongduy2121@gmail.com>
2022-10-11 20:01:13 +0700

    Complete

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser') 
 const cors = require('cors')
 const app = express()
 const port = 3000
 
+
+app.use(cookieParser())
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -12,9 +15,54 @@ app.use(bodyParser.text());
 let connection = []
 let message_all = []
 
+let username = []
 
-app.get('/',(req, res) => {
-    res.sendFile(__dirname + "/index.html")
+let tick = 0
+
+
+app.use((req, res, next) => {
+
+    if (req.cookies.id == undefined || req.cookies.id >= tick) 
+      {
+        res.cookie('id', tick)
+        username.push("")
+        tick++
+      }
+    next()
+})
+
+app.use(express.static(__dirname))
+
+function login(req, res) 
+{
+    return (username[req.cookies.id] != "");
+} 
+
+app.route('/').get((req, res) => {
+
+    if (login(req, res)) res.redirect('/chat')
+    // console.log(login(req,res))       
+    res.sendFile(__dirname + "/views/login_form.html")
+}).post((req, res) => {
+
+    let cookie_id = req.cookies.id
+    // username[]
+    if (req.body.username == "") res.send("Username không được trống")
+    else 
+      {
+        username[cookie_id] = req.body.username
+        res.redirect('/chat')
+      }
+})
+
+
+app.get('/chat',(req, res) => {
+    if (!login(req,res)) res.redirect('/')
+    res.sendFile(__dirname + "/views/index.html")
+})
+
+app.get('/show',(req, res) => {
+    res.send(req.cookies)
 })
 
 let id = 0;
@@ -22,18 +70,19 @@ let id = 0;
 app.get('/get_data',(req, res, next) => {
     res.setHeader("Content-Type", "text/html; charset=utf-8")
     res.setHeader("Transfer-Encoding", "chunked")
-    console.log("request")
-    id++
-    connection.push([res,id])
+    connection.push(res)
 })
 
+app.get('/load',(req, res) => {
+    res.json({"all_chat" : message_all})
+})
 
 app.post('/send',(req, res) => {
-    let text = req.body
+    let text = username[req.cookies.id] + " : " + req.body
+    message_all.push(text)
     connection.map((old_res) => {
-        old_res[0].write(text)
-        old_res[0].end()
-        console.log(old_res[1])
+        old_res.write(text)
+        old_res.end()
     })
     connection = []
     res.end()
---
+++ public/client.js
@@ -0,0 +1,57 @@
+let input = document.getElementById("chat");
+
+
+input.addEventListener("keypress", (event) => {
+    
+    if (event.key === "Enter") 
+        {
+        event.preventDefault();
+        document.getElementById("click_to_submit").click();
+        }
+
+});
+
+
+
+function add_chat(chat_data)
+{
+    let node = document.createElement("li")
+    let textnode = document.createTextNode(chat_data)
+    node.append(textnode)
+    document.getElementById("message").appendChild(node)
+}
+
+
+async function load_all_message()
+{
+    let chat_message = await fetch("/load")
+    let chat_data = await chat_message.json()
+    for (let i in chat_data.all_chat)
+      add_chat(chat_data.all_chat[i])
+}
+
+
+async function subsciber()
+{
+    let chat_message = await fetch("/get_data")
+    let chat_data = await chat_message.text()  
+    add_chat(chat_data)
+    subsciber()
+}
+
+
+function send_chat()
+{
+    let chat_message = document.getElementById("chat").value 
+    document.getElementById("chat").value = ""
+    let url = "/send"
+    let x = fetch(url, {
+        method: "POST",
+        body: chat_message
+    })
+}
+
+load_all_message()
+
+subsciber()
+
